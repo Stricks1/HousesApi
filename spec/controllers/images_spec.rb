1a098fda40e3415a37f6f70203300b2d337b3148
Stricks1 <gmalheiross@gmail.com>
2020-11-16 17:37:17 -0300

    Added test for user and image controllers

---
+++ spec/controllers/images_spec.rb
@@ -0,0 +1,48 @@
+
+require 'rails_helper'
+# rubocop:disable Layout/LineLength
+RSpec.describe 'Images', type: :request do
+  let(:user) { create(:user) }
+  let(:place) { create(:place, user_id: user.id) }
+  let(:im1) { create(:image, place_id: place.id) }
+
+  describe '#Images' do
+    it 'creates an image' do
+      user
+      post '/v1/login', params: { username: 'test', password: '123456789' }
+      parsed_body = JSON.parse(response.body)
+      post '/v1/images', params: { image: { 
+          image_url: "https://hotelgloria.com.br/wp-content/uploads/2019/07/hotel-gloria-blumenau-sc.jpg",
+          place_id: place.id
+        } 
+      }, headers: { 'Authorization' => 'Bearer ' + parsed_body['data']['token'] }
+
+      expect(response).to be_successful
+    end
+
+    it 'saves the new image' do
+      user
+      post '/v1/login', params: { username: 'test', password: '123456789' }
+      parsed_body = JSON.parse(response.body)
+      post '/v1/images', params: { image: { 
+          image_url: "https://hotelgloria.com.br/wp-content/uploads/2019/07/hotel-gloria-blumenau-sc.jpg",
+          place_id: place.id
+        } 
+      }, headers: { 'Authorization' => 'Bearer ' + parsed_body['data']['token'] }
+      expect(response.body).to include('https://hotelgloria.com.br/wp-content/uploads/2019/07/hotel-gloria-blumenau-sc.jpg')
+    end
+
+    it 'deletes an image' do      user
+      post '/v1/login', params: { username: 'test', password: '123456789' }
+      parsed_body = JSON.parse(response.body)
+      post '/v1/images', params: { image: { 
+          image_url: "https://hotelgloria.com.br/wp-content/uploads/2019/07/hotel-gloria-blumenau-sc.jpg",
+          place_id: place.id
+        } 
+      }, headers: { 'Authorization' => 'Bearer ' + parsed_body['data']['token'] }
+      delete "/v1/images/#{Image.last.id}", headers: { 'Authorization' => 'Bearer ' + parsed_body['data']['token'] }
+      expect(response).to be_successful
+    end
+  end
+  # rubocop:enable Layout/LineLength
+end
---
+++ spec/controllers/users_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+# rubocop:disable Layout/LineLength
+
+RSpec.describe 'Users', type: :request do
+  describe '#Users' do
+    it 'creates a user' do
+      post '/v1/users', params: { user: { username: 'test', email: 'test@mail.com', password: '123456789', password_confirmation: '123456789' } }
+      expect(response.body).to include('test')
+    end
+
+    it 'user with same email or username cant be created' do
+      post '/v1/users', params: { user: { username: 'test', email: 'test@mail.com', password: '123456789', password_confirmation: '123456789' } }
+      post '/v1/users', params: { user: { username: 'test', email: 'test2@mail.com', password: '123456789', password_confirmation: '123456789' } }
+      expect(response.body).to include('"username":["has already been taken"]')      
+      post '/v1/users', params: { user: { username: 'test2', email: 'test@mail.com', password: '123456789', password_confirmation: '123456789' } }
+      expect(response.body).to include('"email":["has already been taken"]')
+    end
+  end
+  # rubocop:enable Layout/LineLength
+end
