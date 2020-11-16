ac7171995d944dd03e549ecc7016cc2bc13d6be3
Stricks1 <gmalheiross@gmail.com>
2020-11-15 23:20:34 -0300

    Added authentication token for user

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'puma', '~> 3.11'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'devise'
+gem 'simple_token_authentication', '~> 1.0'
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,6 +1,12 @@
 class V1::SessionsController < ApplicationController
   def create
-  
+    user = User.where(email: params[:email]).first
+
+    if user && user.valid_password?(params[:password])
+      render json: user.as_json(only: [:email, :authentication_token]), status: :created
+    else
+      head(:unauthorized)
+    end
   end
 
   def destroy
