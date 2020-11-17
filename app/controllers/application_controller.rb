cb400dec3091beadf1512054ab9e68c528fb4497
Stricks1 <gmalheiross@gmail.com>
2020-11-17 18:08:20 -0300

    to test why auth not working on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,6 @@ class ApplicationController < ActionController::API
   end
 
   def require_login
-    render json: { message: 'Please Login' }, status: :unauthorized unless logged_in?
+    render json: { message: 'Please Login', user: session_user }, status: :unauthorized unless logged_in?
   end
 end
