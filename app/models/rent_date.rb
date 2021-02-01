5b54e372f1e76aa03e6d688e7f8b6e61bbfcff5e
Stricks1 <gmalheiross@gmail.com>
2021-02-01 14:52:33 -0300

    fixed line size of between

--- app/models/rent_date.rb
+++ app/models/rent_date.rb
@@ -2,7 +2,14 @@ class RentDate < ApplicationRecord
   belongs_to :place
   belongs_to :user
 
-  scope :between, ->(place_id, start_date, end_date) { where('place_id= ? AND ((start_date >= ? AND start_date <= ?) OR (end_date >= ? AND end_date <= ?))', place_id, start_date, end_date, start_date, end_date) }
+  scope :between, ->(place_id, start_date, end_date) { 
+    where('place_id= ? AND ((start_date >= ? AND start_date <= ?) OR (end_date >= ? AND end_date <= ?))',
+    place_id,
+    start_date,
+    end_date,
+    start_date,
+    end_date)
+  }
 
   def price_rent(days)
     place.daily_price * days
